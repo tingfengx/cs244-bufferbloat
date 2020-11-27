4c44614a26123be16ae2450140a38ee66e5f3f5a
tingfengx <tingfeng.xia@outlook.com>
2020-11-27 16:27:04 -0500

    make submission script

---
+++ mksubmit.sh
@@ -0,0 +1,19 @@
+rm -rf ./submit_files
+mkdir ./submit_files
+
+for i in 5 20 100; do 
+  dir=bb-q$i
+  cp $dir/rtt.png submit_files/rtt-$i.png
+  cp $dir/cwnd-iperf.png submit_files/cwnd-$i.png
+  cp $dir/q.png submit_files/q-$i.png
+  cp $dir/download.png submit_files/download-$i.png
+done
+
+cp write_up/pa2.pdf submit_files/pa2.pdf
+cp bufferbloat.py submit_files/bufferbloat.py
+cp plot_queue.py submit_files/plot_queue.py
+cp plot_ping.py submit_files/plot_ping.py
+cp plot_tcpprobe.py submit_files/plot_tcpprobe.py
+cp plot_relationship.py submit_files/plot_relationship.py
+
+cp fit.pdf submit_files/fit.pdf
